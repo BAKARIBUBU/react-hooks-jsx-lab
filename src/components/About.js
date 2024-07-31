5f404a0435505bc88259ba1bc822c158bf19f5bf
bakari-rhoka-bubu <bakaribubu@gmail.com>
2024-07-31 13:58:31 +0300

    jsx

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,15 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>
+      About Me
+    </h2>
+    <p>
+      I am a software developer with a passion for building innovative and user-friendly applications. With a strong
+    </p>
+<img src={image} alt="I made this"/>
+  </div>;
 }
 
 export default About;
